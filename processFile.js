2cf34b57e0bfcb9f3560c2de9fd2e7233719429d
Brandon <idhere703@gmail.com>
2017-07-24 20:47:35 -0400

    Removed console log.

--- processFile.js
+++ processFile.js
@@ -4,5 +4,13 @@
  */
 module.exports = function(data) {
     // Your code goes here.
-
+    console.log(calculateExp(data));
 }
+
+
+// Calculate expenses minus any gains we might have had.
+const calculateExp = (data) => {
+    return data.reduce((tot, line) => {
+        return tot += line['Credit'] ? parseFloat(line['Credit']) : -parseFloat(line['Debit']);
+    }, 0).toFixed(2);
+};
