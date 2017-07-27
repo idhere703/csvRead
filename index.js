d4d34ff632345fb860bec912cd9815ea9345d077
Brandon <idhere703@gmail.com>
2017-07-27 18:13:19 -0400

    Made some adjustments that make the file processor more accessable.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
-const parse = require('csv-parse');
-const async = require('async');
-const moment = require('moment');
 const processFile = require('./processFile');
 
 // If we don't have a file.
@@ -9,42 +5,7 @@ if (process.argv.length < 3) {
     process.exit(1);
 }
 
-
-// Format the data for processing.
-const mapLines = (lines) => {
-    const headers = lines[0];
-    // Remove headers from array.
-    lines.shift();
-    // Set rest of lines.    
-    const data = lines.map(line => {
-        const h = headers;
-        const ret = {};
-        line.forEach((col, index) => {
-            ret[h[index]] = col;
-        });
-        return ret;
-    });
-
-    return data;
-};
-
-// Do any additional necessary processing.
-const processLine = (line) => {
-    // Move onto the next line.
-    return line;
-};
-
-// Setup the parser.
-const parser = parse({
-    delimiter: ','
-}, (err, data) => {
-    const lines = [];
-    async.eachSeries(data, function(line, callback) {
-        lines.push(processLine(line));
-        callback();
-    });
-    const items = mapLines(lines);
-    processFile(items);
+// Your code goes in the callback.
+processFile(process.argv[2], (data) => {
+    console.log('Data', data);
 });
-
-fs.createReadStream(process.argv[2]).pipe(parser);
--- processFile.js
+++ processFile.js
@@ -1,16 +1,51 @@
+const fs = require('fs');
+const parse = require('csv-parse');
+const async = require('async');
+const moment = require('moment');
+
 /* 
  * Data takes the format of an array of objects with the 
  * column headers as the prop accessors.
  */
-module.exports = function(data) {
-    // Your code goes here.
-    console.log(calculateExp(data));
+module.exports = function(file, cb) {
+
+    // Setup the parser.
+    const parser = parse({
+        delimiter: ','
+    }, (err, data) => {
+        const lines = [];
+        async.eachSeries(data, function(line, callback) {
+            lines.push(processLine(line));
+            callback();
+        });
+        const items = mapLines(lines);
+        // Your code goes here.
+        cb(items);
+    });
+
+    fs.createReadStream(file).pipe(parser);
 }
 
+// Format the data for processing.
+const mapLines = (lines) => {
+    const headers = lines[0];
+    // Remove headers from array.
+    lines.shift();
+    // Set rest of lines.    
+    const data = lines.map(line => {
+        const h = headers;
+        const ret = {};
+        line.forEach((col, index) => {
+            ret[h[index]] = col;
+        });
+        return ret;
+    });
 
-// Calculate expenses minus any gains we might have had.
-const calculateExp = (data) => {
-    return data.reduce((tot, line) => {
-        return tot += line['Credit'] ? parseFloat(line['Credit']) : -parseFloat(line['Debit']);
-    }, 0).toFixed(2);
+    return data;
+};
+
+// Do any additional necessary processing.
+const processLine = (line) => {
+    // Move onto the next line.
+    return line;
 };
